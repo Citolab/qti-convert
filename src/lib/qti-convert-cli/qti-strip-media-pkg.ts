6348d61451de211ac13ab1af34fd834bbef119de
Marcel Hoekstra <marcel_hoekstra@outlook.com>
2024-07-09 15:24:10 +0200

    different name of zip file

--- src/lib/qti-convert-cli/qti-strip-media-pkg.ts
+++ src/lib/qti-convert-cli/qti-strip-media-pkg.ts
@@ -6,7 +6,7 @@ const pkg = process.argv[2];
 const mediaTypes = process.argv[3] || 'audio,video';
 
 try {
-  const outputFileName = pkg.replace('.zip', '-no-media.zip');
+  const outputFileName = pkg.replace('.zip', '-stripped.zip');
   const filters = mediaTypes.split(',').map(x => x.trim());
   await removeMediaFromPackage(pkg, outputFileName, filters);
   console.log('Successfully converted the package: ' + outputFileName + '.');
