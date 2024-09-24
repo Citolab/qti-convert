cce9aa0b38d03a20e7fc08195a10c5a5d86f1e72
Marcel Hoekstra <marcel_hoekstra@outlook.com>
2024-09-24 16:54:57 +0200

    cheerio type fix

--- src/lib/qti-transformer/transformers/suffix/index.ts
+++ src/lib/qti-transformer/transformers/suffix/index.ts
@@ -1,7 +1,8 @@
 import * as cheerio from 'cheerio';
+import { Element } from 'domhandler';
 
 export function suffixa($: cheerio.CheerioAPI, elements: string[], suffix: string) {
-  $('*').each((i, el: cheerio.Element) => {
+  $('*').each((i, el: Element) => {
     if (elements.includes(el.name)) {
       el.name = `${el.name}-${suffix}`;
     }
